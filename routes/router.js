8b473f9ed4d0412b3d4bf64cc729a24b28da7e79
TranDat <dattt@mor.com.vn>
2023-05-17 15:31:35 +0700

    Lesson 21,22: build UI register,login,about,...

--- routes/router.js
+++ routes/router.js
@@ -10,8 +10,16 @@ router.get('/about',(req,res)=>{
 router.get('/login',(req,res)=>{
     res.render('login',{pagename:"Login"})
 })
+router.post('/login',(req,res)=>{
+    console.log("Loging ...")
+    res.render('about',{pagename:"About"})
+})
 router.get('/register',(req,res)=>{
     res.render('register',{pagename:"Register"})
+})
+router.post('/register',(req,res)=>{
+    console.log("Rendering Register! ")
+    res.render('home',{pagename:"Home"})
 })
 router.get('/about',(req,res)=>{
     res.render('about',{pagename:"About"})
